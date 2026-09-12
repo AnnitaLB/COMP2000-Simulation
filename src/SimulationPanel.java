be428a5f609c10d476156bb44e9af856d60d56b1
AnnitaLB <bing.liang2@students.mq.edu.au>
2026-09-13 04:23:17 +0800

    rabbit finally noticed there is more than one grass

--- src/SimulationPanel.java
+++ src/SimulationPanel.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SimulationPanel extends JPanel {
 
     private Rabbit rabbit;
     private Wolf wolf;
-    private Grass grass;
+
+    private ArrayList<Grass> grassList;
 
     private Timer timer;
 
@@ -14,10 +16,20 @@ public class SimulationPanel extends JPanel {
 
         rabbit = new Rabbit(200, 200);
         wolf = new Wolf(300, 300);
-        grass = new Grass(100, 100);
+
+        grassList = new ArrayList<Grass>();
+
+        grassList.add(new Grass(100, 100));
+        grassList.add(new Grass(400, 150));
+        grassList.add(new Grass(250, 500));
 
         timer = new Timer(500, e -> {
-            rabbit.moveTowards(grass.getX(),grass.getY());
+            Grass targetGrass = findClosestGrass();
+
+        rabbit.moveTowards(
+            targetGrass.getX(),
+            targetGrass.getY()
+        );
             wolf.moveTowards(rabbit.getX(),rabbit.getY());
 
             rabbit.loseEnergy(1);
@@ -38,7 +50,10 @@ public class SimulationPanel extends JPanel {
         super.paintComponent(g);
 
         g.setColor(Color.GREEN);
+
+        for (Grass grass : grassList) {
         g.fillRect(grass.getX(), grass.getY(), 20, 20);
+    }
 
         g.setColor(Color.GRAY);
         g.fillOval(rabbit.getX(), rabbit.getY(), 20, 20);
@@ -52,16 +67,23 @@ public class SimulationPanel extends JPanel {
         g.drawString("Wolf Energy: " + wolf.getEnergy(), 10, 40);
     }
     
-    private void checkRabbitGrass(){
-        if(rabbit.getX() < grass.getX() + 20 && rabbit.getX() + 20> grass.getX() &&
-            rabbit.getY() < grass.getY() +20 && rabbit.getY() + 20 > grass.getY()) {
+    private void checkRabbitGrass() {
+    for (int i = 0; i < grassList.size(); i++) {
 
-        rabbit.addEnergy(5);
+        Grass grass = grassList.get(i);
 
-        int newX = (int)(Math.random() * 630);
-        int newY = (int)(Math.random() * 630);
+        if (rabbit.getX() < grass.getX() + 20 &&
+            rabbit.getX() + 20 > grass.getX() &&
+            rabbit.getY() < grass.getY() + 20 &&
+            rabbit.getY() + 20 > grass.getY()) {
 
-        grass = new Grass(newX, newY);
+            rabbit.addEnergy(5);
+
+            int newX = (int)(Math.random() * 630);
+            int newY = (int)(Math.random() * 630);
+
+            grassList.set(i, new Grass(newX, newY));
+        }
     }
 }
     private void checkWolfRabbit() {
@@ -92,4 +114,26 @@ public class SimulationPanel extends JPanel {
         wolf = new Wolf(newX, newY);
     }
     }
+    private Grass findClosestGrass() {
+    Grass closestGrass = grassList.get(0);
+
+    int closestDistance =
+        Math.abs(rabbit.getX() - closestGrass.getX()) +
+        Math.abs(rabbit.getY() - closestGrass.getY());
+
+    for (int i = 1; i < grassList.size(); i++) {
+        Grass grass = grassList.get(i);
+
+        int distance =
+            Math.abs(rabbit.getX() - grass.getX()) +
+            Math.abs(rabbit.getY() - grass.getY());
+
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            closestGrass = grass;
+        }
+    }
+
+    return closestGrass;
+}
 }
